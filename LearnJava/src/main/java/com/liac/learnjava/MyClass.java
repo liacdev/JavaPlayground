d11cd2e9f69a9e893cd1b549ea60baac2c64c2aa
liacdev <jagoblacksky@gmail.com>
2022-08-04 05:45:59 -0500

    Lessons 16 notes

--- LearnJava/src/main/java/com/liac/learnjava/MyClass.java
+++ LearnJava/src/main/java/com/liac/learnjava/MyClass.java
@@ -2,6 +2,10 @@ package com.liac.learnjava;
 
 public class MyClass {
     public static void main(String[] args) {
+        String characterName = "Santa Clause";
+        String nullName = null;
+        String emptyName = "";
+
         System.out.println("Hello Crunchy!");
         System.out.println("Hello Schwifty!");
         System.out.println("Hello Rutabaga!");
@@ -10,5 +14,16 @@ public class MyClass {
         System.out.println("Classes are written in PascalCase in Java.");
         System.out.println("Parameters or arguments are passed inside of a method.");
         System.out.println("Strings DO NOT work inside single quotes in Java!");
+        System.out.println("");
+
+        System.out.println("Lesson 17");
+        System.out.println("Variables in Java must be declared with a type and a name");
+        System.out.println("Variables should also be initialized");
+        System.out.println("The most well known Christmas character is " + characterName + ".");
+        System.out.println("A string can also be declared as " + nullName + " or empty " + emptyName + ".");
+        System.out.println("A " + nullName + " value will print the word " + nullName + " when sent to the console.");
+        System.out.println("An empty string is a variable, even though it is empty.");
+        System.out.println("");
+
     }
 }
