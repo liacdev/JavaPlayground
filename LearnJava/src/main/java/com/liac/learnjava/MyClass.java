19f41d6579f0f452226a391ca860800825b6c2fe
liacdev <jagoblacksky@gmail.com>
2022-08-03 19:51:01 -0500

    Lessons 13, 14, and 15 notes

---
+++ LearnJava/src/main/java/com/liac/learnjava/MyClass.java
@@ -0,0 +1,14 @@
+package com.liac.learnjava;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Crunchy!");
+        System.out.println("Hello Schwifty!");
+        System.out.println("Hello Rutabaga!");
+        System.out.println("Let's learn about Java code structure.");
+        System.out.println("The package allows us to organize related classes in Java.");
+        System.out.println("Classes are written in PascalCase in Java.");
+        System.out.println("Parameters or arguments are passed inside of a method.");
+        System.out.println("Strings DO NOT work inside single quotes in Java!");
+    }
+}
