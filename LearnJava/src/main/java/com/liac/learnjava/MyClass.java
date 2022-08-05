79bebc0936c74fa7f64b8cbba7f68e1e6e1df670
liacdev <jagoblacksky@gmail.com>
2022-08-04 22:00:08 -0500

    Lessons 17 notes

--- LearnJava/src/main/java/com/liac/learnjava/MyClass.java
+++ LearnJava/src/main/java/com/liac/learnjava/MyClass.java
@@ -2,10 +2,7 @@ package com.liac.learnjava;
 
 public class MyClass {
     public static void main(String[] args) {
-        String characterName = "Santa Clause";
-        String nullName = null;
-        String emptyName = "";
-
+        System.out.println("Lessons 14 and 15: Java code structure");
         System.out.println("Hello Crunchy!");
         System.out.println("Hello Schwifty!");
         System.out.println("Hello Rutabaga!");
@@ -16,7 +13,11 @@ public class MyClass {
         System.out.println("Strings DO NOT work inside single quotes in Java!");
         System.out.println("");
 
-        System.out.println("Lesson 17");
+        System.out.println("Lesson 17: String variables and concatenation");
+        String characterName = "Santa Clause";
+        String nullName = null;
+        String emptyName = "";
+
         System.out.println("Variables in Java must be declared with a type and a name");
         System.out.println("Variables should also be initialized");
         System.out.println("The most well known Christmas character is " + characterName + ".");
@@ -24,6 +25,18 @@ public class MyClass {
         System.out.println("A " + nullName + " value will print the word " + nullName + " when sent to the console.");
         System.out.println("An empty string is a variable, even though it is empty.");
         System.out.println("");
+
+        System.out.println("Lesson 18: Integer variable type");
+        int  age = 2000;
+        int year = 1998;
+        System.out.println("Hello, my name is " + characterName + " and I am " + age + " years old.");
+        System.out.println("Java source file --> Compiler --> Bytecode (machine instructions) --> Device (with JVM) ");
+        System.out.println("Compiler checks for syntax and errors.");
+        System.out.println("JVM is Java Virtual Machine");
+        System.out.println("The year is " + year);
+        System.out.println("");
+
+        System.out.println("Lesson 18: Java long and double variable types");
 
     }
 }
